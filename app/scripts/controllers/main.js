4def3a8b41518118ad83db2533d3396cdf4a72ef
James Cool <jeffreysbrother@gmail.com>
2015-09-28 15:11:54 -0700

    Yeoman: grunt watch and server

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -16,7 +16,7 @@
     $scope.$watch('todos', function () {
       localStorageService.set('todos', $scope.todos);
     }, true);
-    
+
     $scope.addTodo = function () {
       $scope.todos.push($scope.todo);
       $scope.todo = '';
